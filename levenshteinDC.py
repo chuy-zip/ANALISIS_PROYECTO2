be21dd812b267fc57bc3141a976c073daa86724f
chuy-zip <andreschuy03@gmail.com>
2025-04-02 13:37:24 -0600

    clean examples and dc aproach

--- levenshteinDC.py
+++ levenshteinDC.py
@@ -10,7 +10,28 @@ def load_test_cases(filename):
     return test_cases
 
 def levenshtein_distance_DC(str1, str2):
-    print()
+    # caso baso 1, si la primera cadena esta vacia, el costo/operaciones
+    # que se tendran que hacer para convertir la cadena str1 a str2
+    # es el largo de la cadena 2
+    if len(str1) == 0:
+        return len(str2)
+    
+    # caso base 2, lo mismo que el caso anterior, pero ahora la cadena 2 es vacia, 
+    # entonces la cantidad de operaciones a realizar es el largo de la cadena 1
+    if len(str2) == 0:
+        return len(str1)
+    
+    # si los primeros char son iguales se continua con los demas
+    if str1[0] == str2[0]:
+        return levenshtein_distance_DC(str1[1:], str2[1:])
+    else:
+        # calculo de las 3 opciones posibles recursivamente
+        deletion = levenshtein_distance_DC(str1[1:], str2) + 1
+        insertion = levenshtein_distance_DC(str1, str2[1:]) + 1
+        substitution = levenshtein_distance_DC(str1[1:], str2[1:]) + 1
+        
+        # minimo de las tres opciones
+        return min(deletion, insertion, substitution)
 
 json_data = load_test_cases("strings.json")
 
@@ -31,11 +52,12 @@ for index, strings in enumerate(test_cases, start=1):
     print(f"Caso: {index } con cadenas: '{str1}' y '{str2}' ")
 
     start_time = time.perf_counter()
-    levenshtein_distance_DC(str1, str2)
+    ld = levenshtein_distance_DC(str1, str2)
     end_time = time.perf_counter()
 
     exec_time = end_time - start_time
 
+    print(f"Resultado: {ld}")
     execution_times.append(exec_time)
     indexes.append(index)
 
